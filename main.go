075b05a82aa2703e92dc9e51e933540e7459ff6a
Duncan Rance <drance@veracode.com>
2024-10-04 11:57:20 +0100

    build

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"github.com/mingrammer/go-todo-rest-api-example/app"
 	"github.com/mingrammer/go-todo-rest-api-example/config"
-	"bufio"
-	"fmt"
 	"os"
 )
+
+func check(e error) {
+    if e != nil {
+        panic(e)
+    }
+}
 
 func main() {
 	config := config.GetConfig()
@@ -15,4 +19,5 @@ func main() {
 	app.Run(":3000")
 	d1 := []byte("hello\ngo\n")
 	err := os.WriteFile("/tmp/foobar", d1, 0644)
+	check(err)
 }
